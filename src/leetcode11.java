481cbead3d7273a80ec7c9dfb2600d73ca9afd20
endeavor-zzc <endeavor.zzc@gmail.com>
2025-07-22 16:31:38 +0800

    2025/7/22

---
+++ src/leetcode11.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class leetcode11 {
+    public static void main(String[] args){
+        int result;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("输入数据");
+        String input = scanner.nextLine();
+        String[] strings = input.split(",");
+        int[] nums = new int[strings.length];
+        for (int i = 0; i < strings.length; i++) {
+            nums[i] = Integer.parseInt(strings[i]);
+        }
+        Solution11 solution11 = new Solution11();
+        result = solution11.maxArea(nums);
+        System.out.println(result);
+    }
+}
+class Solution11{
+    public int maxArea(int[] height){
+        int max = 0;
+        int left = 0; //左指针，指向开头
+        int right = height.length - 1;//右指针，指向数组末尾
+        while (left < right){
+            int temp = Math.min(height[left],height[right]) * (right - left);
+            max = Math.max(max, temp);
+            if (height[left] <= height[right])
+                left++;
+            else
+                right--;
+        }
+        return max;
+    }
+}
