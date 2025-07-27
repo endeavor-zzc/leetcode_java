fad165babf1e1a97bfee4b70a077aeebe57deb61
endeavor-zzc <endeavor.zzc@gmail.com>
2025-07-27 16:35:54 +0800

    2025/7/27

---
+++ src/leetcode53.java
@@ -0,0 +1,38 @@
+import java.beans.PropertyEditorSupport;
+import java.util.Scanner;
+
+public class leetcode53 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("输入数组");
+        String input = scanner.nextLine();
+        String[] strings = input.split(",");
+        int[] nums = new int[strings.length];
+        for (int i = 0; i < strings.length; i++) {
+            nums[i] = Integer.parseInt(strings[i]);
+        }
+        Solution53 solution53 = new Solution53();
+        int result = solution53.maxSubArray(nums);
+        System.out.println(result);
+    }
+}
+
+class Solution53{
+    /**
+     * 还是利用前缀和的概念,因为是连续数组，可以考虑使用前缀和
+     * 当前的前缀和减去在这之前的最小前缀和就是这一段数组的最大值。
+     * @param nums
+     * @return
+     */
+    public int maxSubArray(int[] nums){
+        int max = Integer.MIN_VALUE;
+        int presum = 0; // 当前的前缀和
+        int min_presum = 0; //最小的前缀和
+        for (int num : nums) {
+            presum += num;
+            max = Math.max(max, presum - min_presum);
+            min_presum = Math.min(presum, min_presum);
+        }
+        return max;
+    }
+}
