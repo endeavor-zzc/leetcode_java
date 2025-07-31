52479842a299304242f98b0108df442c0b6de94f
endeavor-zzc <endeavor.zzc@gmail.com>
2025-07-31 20:24:26 +0800

    2025/7/31

--- src/leetcode206.java
+++ src/leetcode206.java
@@ -1,2 +1,24 @@
 public class leetcode206 {
+        public static void main(String[] args){
+
+        }
+}
+
+class ListNode206{
+    int val;
+    ListNode206 next;
+    ListNode206(){}
+    ListNode206(int val){
+        this.val = val;
+    }
+    ListNode206(int val, ListNode206 next){
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution206{
+    public ListNode206 reverseList(ListNode206 head){
+
+    }
 }
