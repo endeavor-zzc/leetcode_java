a3190c437ebab0161e63a5aa8c9aeb7c2f03ff4e
endeavor-zzc <endeavor.zzc@gmail.com>
2025-09-09 10:10:24 +0800

    2025/9/9

--- src/ListNode/leetcode21.java
+++ src/ListNode/leetcode21.java
@@ -2,7 +2,16 @@ package ListNode;
 
 public class leetcode21 {
     public static void main(String[] args){
-
+        ListNode21 list1 = new ListNode21(1,new ListNode21(2,new ListNode21(4)));
+        ListNode21 list2 = new ListNode21(2, new ListNode21(5));
+        Solution21_2 solution212 = new Solution21_2();
+        ListNode21 result = solution212.mergeTwoLists(list1, list2);
+        while (result != null){
+            System.out.print(result.val);
+            if (result.next != null)
+                System.out.print("->");
+            result = result.next;
+        }
     }
 }
 
@@ -19,9 +28,15 @@ class ListNode21{
     }
 }
 
-class Solution21{
+class Solution21_1 {
+    /**
+     * 用迭代法，逐个比较
+     * @param list1 第一个链表
+     * @param list2 第二个链表
+     * @return NewList的下一个，即头节点
+     */
     public ListNode21 mergeTwoLists(ListNode21 list1, ListNode21 list2){
-        
+        //新创建一个链表，指向空，表示合并完链表之后的头节点的前一个节点，这样避免单独处理头节点
         ListNode21 NewList = new ListNode21();
         ListNode21 cur = NewList;
         while (list1 != null && list2 != null){
@@ -34,7 +49,26 @@ class Solution21{
             }
             cur = cur.next;
         }
-        cur.next = (list1 != null ? list1 : list2);
+        cur.next = (list1 != null ? list1 : list2); //将剩余的部分连接到NewList的尾部
         return NewList.next;
+    }
+}
+
+class Solution21_2{
+    /**
+     *  递归的方法
+     */
+    public ListNode21 mergeTwoLists(ListNode21 list1, ListNode21 list2){
+        if(list1 == null)
+            return list2;
+        if (list2 == null)
+            return list1;
+        if (list1.val < list2.val){
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        }else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
     }
 }
