295eb9611d27ef058f9359e30959448ffc653f29
endeavor-zzc <endeavor.zzc@gmail.com>
2025-09-21 19:22:16 +0800

    2025/9/21

--- src/binarytree/leetcode101.java
+++ src/binarytree/leetcode101.java
@@ -1,13 +1,40 @@
 package binarytree;
 
+import com.sun.source.tree.Tree;
+
 public class leetcode101 {
     public static void main(String[] args){
-
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(3);
+        Solution101 solution101 = new Solution101();
+        boolean result = solution101.isSymmetric(root);
+        System.out.println(result);
     }
 }
 
 class Solution101{
     public boolean isSymmetric(TreeNode root){
+         return isSameTree(root.left, root.right);
+    }
 
+    /**
+     * 是对相同的树改版
+     * @param p
+     * @param q
+     * @return
+     */
+    private boolean isSameTree(TreeNode p, TreeNode q){
+
+        if (p == null || q == null)
+            return p == q;
+        if (p.val != q.val)
+            return false;
+        else
+            return isSameTree(p.left, q.right) && isSameTree(p.right, q.left);
     }
 }
