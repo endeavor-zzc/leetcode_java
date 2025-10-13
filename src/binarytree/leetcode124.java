3ff4d81074bb63e922b80282e091abfcdc0201c8
endeavor-zzc <endeavor.zzc@gmail.com>
2025-10-13 21:19:13 +0800

    2025/10/13

---
+++ src/binarytree/leetcode124.java
@@ -0,0 +1,27 @@
+package binarytree;
+
+/**
+ * 二叉树中的最大路径和
+ */
+public class leetcode124 {
+    public static void main(String[] args){
+
+    }
+}
+
+class Solution124{
+    int ans = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root){
+        dfs(root);
+        return ans;
+    }
+
+    private int dfs(TreeNode node){
+        if (node == null)
+            return 0;
+        int left = Math.max(node.val, dfs(node.left) + node.val);
+        int right = Math.max(node.val, dfs(node.right) + node.val);
+        ans = Math.max(ans, left + right - node.val);  //这里因为把左右子树加在一起会多算一个root的值，所以要减去
+        return Math.max(left, right);
+    }
+}
